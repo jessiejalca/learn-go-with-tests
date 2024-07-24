a2370f36016d0ff65685b21d35383b4d88d2d27c
Jessie Jalca <jessiejalca@gmail.com>
2024-07-24 10:11:51 -0400

    feat: Sum multiple given arrays

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,24 @@
 package main
 
+// Sum takes an integer array and returns its sum
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+/*
+	 SumAll takes any amount of integer arrays and returns
+		an array of each array's sum
+*/
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
